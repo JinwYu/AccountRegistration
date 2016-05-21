ea55248cec1cf1d27f9f2fb4568039d1d221a28c
Jinwoo <Jinwoo>
2016-05-21 05:33:54 +0200

    fixat lite av lyssnarna

--- app/src/main/java/com/example/jinwoo/accountregistration/AccountRegistration.java
+++ app/src/main/java/com/example/jinwoo/accountregistration/AccountRegistration.java
@@ -1,14 +1,19 @@
 package com.example.jinwoo.accountregistration;
 
 import android.content.Context;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Vector;
 
 /**
  * TODO: SKRIV OM KLASSEN HÄR JAVADOC
@@ -20,6 +25,18 @@ public class AccountRegistration extends LinearLayout {
 
     // Account data variables.
     private String password, username, email, company, firstName, lastName, day, month, year, Gender;
+    private Vector<Boolean> inputComplete;
+    private Vector<Boolean> isMandatory;
+    private int index = 0;
+
+    /*
+        TODO: TROR INDEX ÄR FEL, kolla print outsen.
+
+        TODO: Getters för alla variabler.
+
+
+     */
+
 
     public AccountRegistration(Context context) {
         super(context);
@@ -52,6 +69,9 @@ public class AccountRegistration extends LinearLayout {
         month = "";
         year = "";
 
+        inputComplete = new Vector<Boolean>();
+        isMandatory = new Vector<Boolean>();
+
         // Layout parameters.
         linearLayoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         layoutParamsWrap = new ViewGroup.LayoutParams(270, ViewGroup.LayoutParams.WRAP_CONTENT); //TODO: hårdkodat
@@ -70,8 +90,6 @@ public class AccountRegistration extends LinearLayout {
 
     // Return the account registration layout.
     public LinearLayout getRegistrationLayout(){
-        // TODO: kanske blir tvungen att lägga bools om ett fält skapats i mina createField-funktioner. Så man vet vilka som ska addas till fieldContainer
-
 
         // Add the "register" button.
         Button registerButton = new Button(getContext());
@@ -79,13 +97,38 @@ public class AccountRegistration extends LinearLayout {
         registerButton.setGravity(Gravity.CENTER);
         ViewGroup.LayoutParams buttonParams = new ViewGroup.LayoutParams(600, ViewGroup.LayoutParams.WRAP_CONTENT);
         registerButton.setLayoutParams(buttonParams);
+
+        // Button listener.
+        registerButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If all of the mandatory fields have been inputted then save the inputs.
+                for(int i = 0; i < isMandatory.size(); i++){
+
+                    // TODO: Basically, if all of the inputComplete are true at the indexes that should be true, save input.
+                    if(isMandatory.get(i)){
+                        if(inputComplete.get(i)){
+                            System.out.println("Saved user inputs.");
+                            // TODO: BUT HOW? när editText.getText är inne i createTextField?
+                        }
+
+                    }
+                }
+
+            }
+        });
+
+        // Add the button to the view.
         fieldContainer.addView(registerButton);
 
         return fieldContainer;
     }
 
-    public void createTextField(String label, boolean isMandatory){
+    public void createTextField(String label, final boolean mandatory){
         // TODO: Vector<Strings> so we can add variables dynamically.
+
+        // Keep track of the mandatory fields.
+        isMandatory.add(index, mandatory);
 
         // Temp linear layout.
         LinearLayout linearLayout = new LinearLayout(getContext());
@@ -110,20 +153,42 @@ public class AccountRegistration extends LinearLayout {
             editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
         }
 
+        // Add a listener.
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // If the user input has been inputted.
+                if(s.length() == 4) {
+                    System.out.println("User input complete!");
+                    inputComplete.addElement(true);
+                }
+                else{
+                    System.out.println("Index = " + index);
+                    if(!inputComplete.isEmpty() && inputComplete.get(index) == null){
+                        inputComplete.addElement(false);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         // Add the views the layout.
         linearLayout.addView(textView);
         linearLayout.addView(editText);
-
         fieldContainer.addView(linearLayout);
 
-        System.out.println("");
-
-        // TODO: isMandatory, hur?
-
-
+        index++;
     }
 
-    // TODO: The below ones will use a separate xml-file.
     public void createEmailField(boolean isMandatory){
         createTextField("Email", isMandatory);
     }
@@ -135,10 +200,4 @@ public class AccountRegistration extends LinearLayout {
     public void createBirthField(boolean isMandatory){
         // Parse string to ints.
     }
-
-    // TODO: En funktion som skapar ett account när alla mandatory fields är ifyllda. Lyssnare för textfields?
-
-    // TODO: Getters för alla variabler.
-
-
 }
